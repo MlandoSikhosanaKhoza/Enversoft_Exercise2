fe6a9eb2281288619a48b2eab90673def23b58c9
mlando2.0 <mlando2.0@outlook.com>
2023-07-09 09:06:41 +0200

    ReadService Created

---
+++ Enversoft_Exercise2.Software/Entities/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enversoft_Exercise2.Software
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
--- Enversoft_Exercise2.Software/Services/ReadCsvService.cs
+++ Enversoft_Exercise2.Software/Services/ReadCsvService.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using CsvHelper;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Enversoft_Exercise2.Software.Services
 {
-    internal class ReadCsvService
+    public class ReadCsvService : IReadCsvService
     {
+        public List<Person> GetPeople()
+        {
+            List<Person> people = new List<Person>();
+            StreamReader streamReader = new StreamReader("./DataCSV/Data.csv");
+            using(var csv=new CsvReader(streamReader,CultureInfo.InvariantCulture))
+            {
+                people=csv.GetRecords<Person>().ToList();
+            }
+            
+            return people;
+        }
     }
 }
