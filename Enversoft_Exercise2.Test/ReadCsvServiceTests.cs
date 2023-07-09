b3503ec973bd046f893ba00560a0aae1d8ae8cd7
mlando2.0 <mlando2.0@outlook.com>
2023-07-09 09:13:00 +0200

    Fake It Easy and Fluent Assertions added (NuGet)

---
+++ Enversoft_Exercise2.Test/ReadCsvServiceTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enversoft_Exercise2.Test
+{
+    public class ReadCsvServiceTests
+    {
+        private readonly ReadCsvServiceTests _readCsvService;
+        public ReadCsvServiceTests() { 
+            
+        }
+    }
+}
