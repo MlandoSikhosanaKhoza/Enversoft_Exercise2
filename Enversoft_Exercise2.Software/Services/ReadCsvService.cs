32567bf1bb79927b998a3675740f67c695a47eea
mlando2.0 <mlando2.0@outlook.com>
2023-07-09 08:47:37 +0200

    Add project files.

---
+++ Enversoft_Exercise2.Software/Services/ReadCsvService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enversoft_Exercise2.Software.Services
+{
+    internal class ReadCsvService
+    {
+    }
+}
---
+++ Enversoft_Exercise2.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Enversoft_Exercise2.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
